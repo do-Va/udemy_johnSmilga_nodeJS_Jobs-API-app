9b52a2d90852ebaee3bfc4145105d5566c1c6c8a
do-Va <varisdogukan@outlook.com>
2022-06-30 11:59:26 +0300

    Updated createjob controller.

--- controllers/jobs.js
+++ controllers/jobs.js
@@ -1,13 +1,24 @@
+const Job = require("../models/job");
+const { StatusCodes } = require("http-status-codes");
+const { BadRequestError, NotFoundError } = require("../errors");
+
 const getAllJobs = async (req, res) => {
-  res.send("getAllJobs user");
+  res.send("Get All Jobs");
 };
 
 const getJob = async (req, res) => {
-  res.send("getJob user");
+  res.send("Get Single Job");
 };
 
 const createJob = async (req, res) => {
-  res.json(req.user);
+  // Job oluşturacak kişinin id'sini istek bölümünden alıp createdBy değişkenimize aktarıyoruz.
+  req.body.createdBy = req.user.userId;
+
+  // istek body'si içerisinde gelen bilgiler ile job oluşturuyoruz ve job değişkenine atıyoruz.
+  const job = await Job.create(req.body);
+
+  // Sonrasında oluşan job' elemanını cevap olarak created status code'u ile gönderiyoruz.
+  res.status(StatusCodes.CREATED).json({ job });
 };
 
 const updateJob = async (req, res) => {
